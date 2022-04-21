5b705a8b825990765a90a6e9bd0150dad5524ecb
Yash Raj Singh <yashrajsingh4799@gmail.com>
2022-04-21 17:49:36 +0530

    few packages added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import './App.css';
+import logo from "./logo.svg";
+import { Navbar, NavbarBrand } from "reactstrap";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <Navbar dark color="primary">
-      <div className="container">
-      <NavbarBrand href="/">
-      Yash Raj Singh
-      </NavbarBrand>
-      </div>
-
-      </Navbar>  
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Yash Raj Singh</NavbarBrand>
+        </div>
+      </Navbar>
     </div>
+    
   );
 }
 
